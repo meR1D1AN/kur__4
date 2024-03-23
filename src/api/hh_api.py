bcfff93db57804ea8500a44ca24eea6f20cda2bf
Nikita <nikita@mer1d1an.ru>
2024-03-23 16:11:36 +0300

    добавление пояснения к методам

--- src/api/hh_api.py
+++ src/api/hh_api.py
@@ -4,15 +4,32 @@ import requests
 
 # Класс для работы с API платформы hh.ru
 class HeadHunterAPI(VacancyAPI):
+    """
+    Инициализация API hh.ru
+    """
+
     def __init__(self):
         super().__init__("https://api.hh.ru/")
         self._url = self.base_url
 
     @property
     def url(self):
+        """
+        Геттер для получения URL API hh.ru
+        :return: str: URL API hh.ru
+        """
         return self._url
 
     def get_vacancies(self, search_query, area=None, page=0):
+        """
+        Получает список вакансий с API hh.ru
+            search_query (str): Поисковый запрос.
+            area (str, optional): Код региона для поиска вакансий. По умолчанию None.
+            page (int, optional): Номер страницы результатов. По умолчанию 0.
+        :return:
+            dict: Словарь с результатами поиска вакансий.
+            None: В случае ошибки при выполнении запроса.
+        """
         url = f"{self.base_url}vacancies"
         params = {
             "text": search_query,
